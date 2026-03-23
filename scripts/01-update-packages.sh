e0592c4397e6a008df422c8232362a0eec4de434
Cleboost <clement.balarot@gmail.com>
2026-03-23 20:40:37 +0100

    feat: add gpu-screen-recorder to package update script

--- scripts/01-update-packages.sh
+++ scripts/01-update-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # shellcheck disable=SC2054
-ALL_PKGS=("stow" "thunar" "kitty" "noctalia-shell-git" "noctalia-qs-git" "base-devel" "git", "starship")
+ALL_PKGS=("stow" "thunar" "kitty" "noctalia-shell-git" "noctalia-qs-git" "base-devel" "git" "starship" "gpu-screen-recorder")
 
 if command -v yay &> /dev/null; then
     HELPER="yay"
